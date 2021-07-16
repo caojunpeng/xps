b4066bcfbd919e2e84e5c7de91a00375044b25dc
caojunpeng <1162408885@qq.com>
2021-07-16 10:11:12 +0800

    新建数据库

---
+++ xps-app/src/main/resources/sql/update1.0.1/data-update1.0.1.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user`(`user_id`, `user_name`, `user_pwd`, `phone`, `email`, `name`, `create_time`, `update_time`, `login_last`, `login_total`, `creator`) VALUES (1, '1', '1', '1', '1', '1', '2021-07-13 17:16:49', NULL, NULL, NULL, NULL);
